620cd1add51b38cea0244b4ea274c37f6722ebdc
Dmitry Yurov <yurov37@gmail.com>
2023-02-15 22:04:40 +0200

    Update index.js

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,13 +1,3 @@
-// const addContactValidation = require("./validation");
-// const updateContactValidation = require("./validation");
-// const updateContactFavoriteValidation = require("./validation");
-// const {
-//   //   addContactValidation,
-//   //   updateContactFavoriteValidation,
-//   //   updateContactValidation,
-//   validation,
-// } = require("./validation");
-
 const validation = require("./validation");
 const isValidId = require("./isValidId");
 module.exports = {
