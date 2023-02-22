46e6c9a9acf464a73e75c19ea6e51c559432f5de
Dmitry Yurov <yurov37@gmail.com>
2023-02-22 19:03:35 +0200

    processing avatar with jimp

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,6 +1,7 @@
 const { User } = require("../../models");
 const path = require("path");
 const fs = require("fs").promises;
+const jimp = require("jimp");
 
 const updateAvatar = async (req, res) => {
   const { path: tmpUpload, originalname } = req.file;
@@ -14,6 +15,16 @@ const updateAvatar = async (req, res) => {
       "avatars",
       uniqName
     );
+    const img = await jimp.read(tmpUpload);
+    await img
+      .autocrop()
+      .cover(
+        250,
+        250,
+        jimp.HORIZONTAL_ALIGN_CENTER || jimp.VERTICAL_ALIGN_MIDDLE
+      )
+      .writeAsync(tmpUpload);
+
     await fs.rename(tmpUpload, resultUpload);
     const avatarURL = path.join("public", "avatars", uniqName);
     await User.findByIdAndUpdate(id, { avatarURL });
